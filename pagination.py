99c19aa2a87781669ecc96141e8643bdbb5aefcf
Joao Valverde <acci.valverde@gmail.com>
2018-12-04 22:06:13 +0000

    tweaked simple pagination class

--- pagination.py
+++ pagination.py
@@ -1,3 +1,8 @@
+from collections import OrderedDict
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
+
+
 class SimplePagination(PageNumberPagination):
     page_size = 14
     page_size_query_param = 'page_size'
@@ -5,11 +10,10 @@ class SimplePagination(PageNumberPagination):
 
     def get_paginated_response(self, data):
         return Response(OrderedDict([
-            ('item_count', self.page.paginator.count),
+            ('count', self.page.paginator.count),
             ('page_count', self.page.paginator.num_pages),
             ('current', self.page.number),
             ('next', self.page.next_page_number() if self.page.has_next() else None),
             ('previous', self.page.previous_page_number() if self.page.has_previous() else None),
             ('results', data),
         ]))
-
